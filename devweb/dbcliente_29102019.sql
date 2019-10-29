09a48e805e60d8b797dcbb1736b9f9a7c1d3a256
Renato Monteiro Batista <r3n4t0@gmail.com>
2019-10-29 20:40:38 -0300

    Modelo de banco de dados dbcliente

---
+++ devweb/dbcliente_29102019.sql
@@ -0,0 +1,215 @@
+﻿-- phpMyAdmin SQL Dump
+-- version 4.7.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: 29-Out-2019 às 23:27
+-- Versão do servidor: 5.6.34
+-- PHP Version: 7.1.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `dbcliente`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `cliente`
+--
+
+CREATE TABLE `cliente` (
+  `id` bigint(11) NOT NULL,
+  `nomecliente` varchar(50) NOT NULL,
+  `tipocliente_id` int(11) NOT NULL,
+  `dtnasc` date DEFAULT NULL,
+  `cpf` varchar(11) DEFAULT NULL,
+  `endereco` varchar(50) DEFAULT NULL,
+  `bairro` varchar(50) DEFAULT NULL,
+  `cidade` varchar(50) DEFAULT NULL,
+  `cep` varchar(9) DEFAULT NULL,
+  `tel` varchar(20) DEFAULT NULL,
+  `cel` varchar(20) DEFAULT NULL,
+  `email` varchar(150) DEFAULT NULL,
+  `sexo` varchar(20) DEFAULT NULL,
+  `limite` decimal(10,0) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `cliente`
+--
+
+INSERT INTO `cliente` (`id`, `nomecliente`, `tipocliente_id`, `dtnasc`, `cpf`, `endereco`, `bairro`, `cidade`, `cep`, `tel`, `cel`, `email`, `sexo`, `limite`) VALUES
+(1, 'RITA', 1, '0000-00-00', '222.222.222', NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'Masculino', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `itemvenda`
+--
+
+CREATE TABLE `itemvenda` (
+  `id` bigint(20) NOT NULL,
+  `produto_id` bigint(20) NOT NULL,
+  `venda_id` bigint(20) NOT NULL,
+  `quantidade` int(11) NOT NULL,
+  `valorunitario` decimal(10,0) NOT NULL,
+  `valordesconto` decimal(10,0) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `produto`
+--
+
+CREATE TABLE `produto` (
+  `id` bigint(20) NOT NULL,
+  `nomeproduto` varchar(50) NOT NULL,
+  `qtdestoque` int(11) NOT NULL,
+  `estoqueminimo` int(11) NOT NULL,
+  `valorvenda` decimal(10,0) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tipocliente`
+--
+
+CREATE TABLE `tipocliente` (
+  `id` int(11) NOT NULL,
+  `nometipo` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `tipocliente`
+--
+
+INSERT INTO `tipocliente` (`id`, `nometipo`) VALUES
+(1, 'NORMAL'),
+(3, 'DEVEDOR'),
+(4, 'VIP');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `venda`
+--
+
+CREATE TABLE `venda` (
+  `id` bigint(20) NOT NULL,
+  `cliente_id` bigint(20) NOT NULL,
+  `datavenda` date NOT NULL,
+  `valorvenda` decimal(10,0) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `cliente`
+--
+ALTER TABLE `cliente`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `nometipo_id` (`tipocliente_id`);
+
+--
+-- Indexes for table `itemvenda`
+--
+ALTER TABLE `itemvenda`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `item_venda_fk1` (`venda_id`),
+  ADD KEY `item_produto_fk1` (`produto_id`);
+
+--
+-- Indexes for table `produto`
+--
+ALTER TABLE `produto`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tipocliente`
+--
+ALTER TABLE `tipocliente`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `venda`
+--
+ALTER TABLE `venda`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `venda_cliente_fk1` (`cliente_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `cliente`
+--
+ALTER TABLE `cliente`
+  MODIFY `id` bigint(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `itemvenda`
+--
+ALTER TABLE `itemvenda`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `produto`
+--
+ALTER TABLE `produto`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tipocliente`
+--
+ALTER TABLE `tipocliente`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `venda`
+--
+ALTER TABLE `venda`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Limitadores para a tabela `cliente`
+--
+ALTER TABLE `cliente`
+  ADD CONSTRAINT `fk_tipocliente_cliente` FOREIGN KEY (`tipocliente_id`) REFERENCES `tipocliente` (`id`);
+
+--
+-- Limitadores para a tabela `itemvenda`
+--
+ALTER TABLE `itemvenda`
+  ADD CONSTRAINT `item_produto_fk1` FOREIGN KEY (`produto_id`) REFERENCES `produto` (`id`),
+  ADD CONSTRAINT `item_venda_fk1` FOREIGN KEY (`venda_id`) REFERENCES `venda` (`id`);
+
+--
+-- Limitadores para a tabela `venda`
+--
+ALTER TABLE `venda`
+  ADD CONSTRAINT `venda_cliente_fk1` FOREIGN KEY (`cliente_id`) REFERENCES `cliente` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
